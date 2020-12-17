e01a8c824fa2cde147cd68178dc6ceaeaa9a4e4c
ANJUVIGIL <75435342+ANJUVIGIL@users.noreply.github.com>
2020-12-17 12:58:25 +0530

    Update admin.py

--- Finaldmez/dmezapp/admin.py
+++ Finaldmez/dmezapp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from dmezapp.models import Product,Today,Best, newregis, Customer, Order, OrderItem, ShippingAddress
+from dmezapp.models import Product,Today,Best,Covid, newregis, Customer, Order, OrderItem, ShippingAddress
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Today)
+dmin.site.register(Covid)
 admin.site.register(Best)
 admin.site.register(newregis)
 admin.site.register(Order)
